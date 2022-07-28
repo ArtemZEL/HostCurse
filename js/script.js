78453fafb7a0ebc27eb8ab97e0bf22d8bd4388a3
ArtemZEL <artemzelenok60@gmail.com>
2022-07-28 16:58:22 +0300

    curs60

--- js/script.js
+++ js/script.js
@@ -200,30 +200,37 @@ window.addEventListener('DOMContentLoaded', function() {
     //         new MenuCard(img,altimg,title,descr,price,'.menu .container').render();
     //     });
     // });
-    getResource('http://localhost:3000/menu')
-    .then(data=>createCard(data));
-    function createCard(data){
-        data.forEach(({img,altimg,title,descr,price})=>{
-            const element=document.createElement('div');
 
-            element.classList.add('menu__item');
-
-            element.innerHTML=`
-                <img src=${img} alt=${altimg}>
-                <h3 class="menu__item-subtitle"> ${title}</h3>
-                <div class="menu__item-descr">${descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${price}</span> грн/день</div>
-                </div>`;
-
-            document.querySelector('.menu .container').append(element);    
+    //библиотека axios
+    axios.get('http://localhost:3000/menu')
+    .then(data => {
+        data.data.forEach(({img,altimg,title,descr,price})=>{
+            new MenuCard(img,altimg,title,descr,price,'.menu .container').render();
         });
-    }
+    });
 
+    //2 вариант
+    // getResource('http://localhost:3000/menu')
+    // .then(data=>createCard(data));
+    // function createCard(data){
+    //     data.forEach(({img,altimg,title,descr,price})=>{
+    //         const element=document.createElement('div');
 
+    //         element.classList.add('menu__item');
 
+    //         element.innerHTML=`
+    //             <img src=${img} alt=${altimg}>
+    //             <h3 class="menu__item-subtitle"> ${title}</h3>
+    //             <div class="menu__item-descr">${descr}</div>
+    //             <div class="menu__item-divider"></div>
+    //             <div class="menu__item-price">
+    //                 <div class="menu__item-cost">Цена:</div>
+    //                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
+    //             </div>`;
+
+    //         document.querySelector('.menu .container').append(element);    
+    //     });
+    // }
 
     // new MenuCard(
     //     "img/tabs/vegy.jpg",
@@ -376,9 +383,9 @@ window.addEventListener('DOMContentLoaded', function() {
     // })
     // .then(response => response.json())
     // .then(json => console.log(json));
-    fetch('http://localhost:3000/menu')
-    .then(data=>data.json())
-    .then(res=>console.log(res));
+    // fetch('http://localhost:3000/menu')
+    // .then(data=>data.json())
+    // .then(res=>console.log(res));
 
 
 
