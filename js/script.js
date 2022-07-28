a9537fdf8c0cb36220e126cd6512fe3a1832067e
ArtemZEL <artemzelenok60@gmail.com>
2022-07-28 17:52:15 +0300

    curs61_1variant_Slider

--- js/script.js
+++ js/script.js
@@ -208,7 +208,6 @@ window.addEventListener('DOMContentLoaded', function() {
             new MenuCard(img,altimg,title,descr,price,'.menu .container').render();
         });
     });
-
     //2 вариант
     // getResource('http://localhost:3000/menu')
     // .then(data=>createCard(data));
@@ -386,9 +385,54 @@ window.addEventListener('DOMContentLoaded', function() {
     // fetch('http://localhost:3000/menu')
     // .then(data=>data.json())
     // .then(res=>console.log(res));
+    
+    
+    //Slider
+    //1-метод создания слайдера простой но когда делал произошел сбой пришлось с инета брать
+    let slideIndex = 1;
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
 
+    showSlides(slideIndex);
 
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
 
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
 
+        slides.forEach((item) => item.style.display = 'none');
 
-}); 
+        slides[slideIndex - 1].style.display = 'block';
+        
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    }
+
+    function plusSlides (n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', function(){
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function(){
+        plusSlides(1);
+    });
+    //2 способ
+});
